23c85e171071c991a030e09987bac1d85365f54a
Kevin Frankot <kevinfrankot27@gmail.com>
2024-08-24 10:32:23 -0700

    feat: builds for cjs and esm

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,8 @@ import { babel } from '@rollup/plugin-babel'
 import includePaths from 'rollup-plugin-includepaths'
 import license from 'rollup-plugin-license'
 import terser from '@rollup/plugin-terser'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
 
 const banner = `
 @preserve
@@ -11,10 +13,16 @@ Kevin Frankot - shared-intervals v<%= pkg.version %>
 export default [
   {
     input: 'src/index.ts',
-    output: {
-      file: 'dist/index.js',
-      format: 'es',
-    },
+    output: [
+      {
+        file: 'dist/index.cjs.js',
+        format: 'cjs',
+      },
+      {
+        file: 'dist/index.esm.js',
+        format: 'esm',
+      },
+    ],
     plugins: [
       license({
         banner,
@@ -22,6 +30,8 @@ export default [
       includePaths({
         extensions: ['.ts', '.js'],
       }),
+      resolve(),
+      commonjs(),
       babel({
         babelHelpers: 'runtime',
         exclude: 'node_modules/**',
